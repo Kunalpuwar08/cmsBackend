eea5a42738cea475393ded4a54754c80bd722b7c
Kunalpuwar08 <kapuwar29@gmail.com>
2024-03-19 10:41:10 +0530

    - env added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ const express = require("express");
 const admin = require("firebase-admin");
 require("dotenv").config();
 
-const credentials = require("../service.json");
+const credentials = {
+  type: process.env.TYPE,
+  private_key_id: process.env.private_key_id,
+  private_key: process.env.private_key,
+  client_email: process.env.client_email,
+  client_id: process.env.client_id,
+  auth_uri: process.env.auth_uri,
+  token_uri: process.env.token_uri,
+  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  client_x509_cert_url: process.env.client_x509_cert_url,
+  universe_domain: process.env.universe_domain,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
@@ -30,8 +41,6 @@ mongoose
   .catch((err) => {
     console.log("DB Connection Failed");
   });
-
-
 
 const ipMiddleware = require("express-ip");
 const { ipChecker } = require("./common");

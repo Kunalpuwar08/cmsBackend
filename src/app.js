bae7b5aa15eb41fd9b4fb2f85f06814f49ab7920
Kunalpuwar08 <kapuwar29@gmail.com>
2024-03-19 11:11:29 +0530

    - updated env

--- src/app.js
+++ src/app.js
@@ -5,15 +5,16 @@ require("dotenv").config();
 
 const credentials = {
   type: process.env.TYPE,
-  private_key_id: process.env.private_key_id,
-  private_key: process.env.private_key,
-  client_email: process.env.client_email,
-  client_id: process.env.client_id,
-  auth_uri: process.env.auth_uri,
-  token_uri: process.env.token_uri,
-  auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-  client_x509_cert_url: process.env.client_x509_cert_url,
-  universe_domain: process.env.universe_domain,
+  project_id:process.env.PROJECTID,
+  private_key_id:process.env.PRIVATEKEYID,
+  private_key:process.env.PRIVATEKEY.replace(/\\n/g, '\n'),
+  client_email:process.env.CLIENTEMAIL,
+  client_id:process.env.CLIENTID,
+  auth_uri:process.env.AUTHURI,
+  token_uri:process.env.TOKENURI,
+  auth_provider_x509_cert_url:process.env.AUTHPROVIDERURI,
+  client_x509_cert_url:process.env.CLIENTX50URI,
+  universe_domain:process.env.UNIVERSEDOMAIN,
 };
 
 admin.initializeApp({

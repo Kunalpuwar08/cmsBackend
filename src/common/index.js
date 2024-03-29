32c8938a32828bd45d7ab6395d3c4a7532443eae
Kunalpuwar08 <kapuwar29@gmail.com>
2024-03-29 10:20:43 +0530

    - token time change

--- src/common/index.js
+++ src/common/index.js
@@ -47,7 +47,7 @@ function verifyToken(req, res, next) {
 }
 
 function generateToken(data) {
-  return jwt.sign(data, jwtKey, { expiresIn: "1w" });
+  return jwt.sign(data, jwtKey, { expiresIn: "1h" });
 }
 
 function ipChecker(req, res, next) {
